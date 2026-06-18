b5245c1c8ce842fe9db679ba23db8aa6fb5803a6
= <=>
2026-06-18 15:23:19 -0400

    Main

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sys
+
+
 def main():
-    print("Hello from Web Scraper in Python!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    BASE_URL = sys.argv[1]
+    print(f"starting crawl of: {BASE_URL}")
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
